fbd9018629ae9ce313285c3c1415ae5540c19278
Michael Doyle <mdoyle@splunk.com>
2026-08-21 09:00:18 -0400

    Add POST method to retry_strategy modified:   src/doyles_sdk/_classes.py

--- src/doyles_sdk/_classes.py
+++ src/doyles_sdk/_classes.py
@@ -572,6 +572,7 @@ class SplunkSession(DoyleClass, requests.Session):
                 "GET",
                 "PUT",
                 "PATCH",
+                "POST",
                 "DELETE",
                 "OPTIONS",
                 "TRACE",
